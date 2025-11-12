760c56825341d97b3c4b7d3fae43f4eca6b45191
Renato Berrocal <rberrocal@belocode.com>
2025-11-11 22:49:47 -0500

    Fix appointment detail schema

--- src/app/entities/dtos/output/patientAppointmentDetail.output.dto.ts
+++ src/app/entities/dtos/output/patientAppointmentDetail.output.dto.ts
@@ -1,11 +1,34 @@
 import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
 import { z } from 'zod';
 
+import {
+  AppointmentDocumentIcon,
+  AppointmentDocumentTitle,
+} from 'src/general/enums/appointmentDocumentCategories.enum';
+
 import { PatientAppointmentOutputDTOSchema } from './patientAppointment.output.dto';
 
 extendZodWithOpenApi(z);
 
 export const PatientAppointmentDetailOutputDTOSchema = PatientAppointmentOutputDTOSchema.extend({
+  documents: z
+    .array(
+      z.object({
+        documentId: z.string().openapi({
+          description: 'Unique ID of the document',
+          example: '#b731d7bf-edea-cdce-1da3-08dd0351629c',
+        }),
+        title: z.enum(AppointmentDocumentTitle).optional().openapi({
+          description: 'Title of the document category',
+          example: AppointmentDocumentTitle.PRESCRIPTION,
+        }),
+        icon: z.enum(AppointmentDocumentIcon).optional().openapi({
+          description: 'Icon of the document category',
+          example: AppointmentDocumentIcon.PRESCRIPTION,
+        }),
+      }),
+    )
+    .optional(),
   siteds: z
     .object({
       base64: z.string().openapi({
