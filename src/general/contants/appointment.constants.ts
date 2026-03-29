e48c2acd8ad173634c6e2a0fbd6802eabbe06a07
Renato Berrocal <rberrocal@belocode.com>
2026-03-28 21:56:31 -0500

    feat: general fixes

--- src/general/contants/appointment.constants.ts
+++ src/general/contants/appointment.constants.ts
@@ -5,7 +5,7 @@ export const AppointmentConstants = {
     'Recuerda que toda atención es presentando el DNI o CE físico.',
   ],
   CREATE_REASON: 'Reserva hecha desde el App',
-  CURRENT_MONTHS_LIST: 6,
+  CURRENT_MONTHS_LIST: 12,
   HISTORIC_MONTHS_LIST: -12,
   REASON_ID: '21',
   DEFAULT_DOCUMENT_COUNT: '0',
