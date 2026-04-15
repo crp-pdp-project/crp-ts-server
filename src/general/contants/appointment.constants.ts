98c078d81caafb63557df2a267598267903effee
Renato Berrocal <rberrocal@belocode.com>
2026-04-15 01:59:15 -0500

    feat: update texts and dependencies

--- src/general/contants/appointment.constants.ts
+++ src/general/contants/appointment.constants.ts
@@ -1,9 +1,4 @@
 export const AppointmentConstants = {
-  DEFAULT_RECOMMENDATIONS: [
-    'Llegar 30 min. antes de tu cita.',
-    'En caso de haber realizado el pago dirígete al piso de tu especialidad.',
-    'Recuerda que toda atención es presentando el DNI o CE físico.',
-  ],
   CREATE_REASON: 'Reserva hecha desde el App',
   CURRENT_MONTHS_LIST: 12,
   HISTORIC_MONTHS_LIST: -12,
